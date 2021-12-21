505e75fd978f5102801ba49538da1ee1162ac845
Abhijith Sajimon <95603734+Abhijith-Sajimon@users.noreply.github.com>
2021-12-21 15:43:10 +0530

    WordSearchV3.0
    Used return statement instead of explicit exit() when possible.
    Removed unneeded variables.
    Corrected the placement of curly braces and added spaces wherever needed.
    Corrected the indentation.

--- Main.java
+++ Main.java
@@ -5,41 +5,40 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        if(args.length!=2)
-        {
+        if (args.length != 2) {
             System.out.println("File path and search word cannot be empty");
-            System.exit(0);
+            return;
         }
-        String path=args[0];
-        String search=args[1];
-        File file=new File(path);
+        String path = args[0];
+        String search = args[1];
+        File file = new File(path);
         System.out.println("Processing...");
-        doesFileExist(file);
-        wordCount(file,search);
-    }
-
-    private static void doesFileExist(File file)
-    {
-        if (!file.isFile())
-        {
-            System.out.println("File does not exist");
-        } else isFileFormatSupported(file);
-    }
-
-    private static void isFileFormatSupported(File file)
-    {
-        if (!file.getName().endsWith("txt"))
-        {
-            System.out.println("File format not supported");
+        validateFile(file);
+        if (!validateFile(file)) {
+            wordCount(file, search);
         }
     }
 
-    private static int wordCount(File file,String search){
+    private static boolean validateFile(File file) {
 
-        String data = null;
-        String removeSpecialChar="";
+        if (!file.exists()) {
+            System.out.println("File does not exist");
+            System.exit(0);
+        } else {
+            String fullName = file.getName();
+            if (!(fullName.endsWith(".txt") || fullName.endsWith(".json"))) {
+                System.out.println("File format not supported");
+                System.exit(0);
+            }
+        }
+        return false;
+    }
+
+    private static void wordCount(File file, String search) {
+
+        String data = "";
         try {
             FileReader reader = new FileReader(file);
             BufferedReader buffer = new BufferedReader(reader);
@@ -47,19 +46,17 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(data!=null) {
-                removeSpecialChar = data.replaceAll("\\p{Punct}", "");
-        }else {
-                System.out.println("Empty file");
-                System.exit(1);
-            }
-        StringTokenizer st = new StringTokenizer(removeSpecialChar);
+        if (data != null) {
+            data = data.replaceAll("\\p{Punct}", "");
+        } else {
+            System.out.println("Empty file");
+            return;
+        }
+        StringTokenizer st = new StringTokenizer(data);
         int wordcount = 0;
-        String temp = "";
         while (st.hasMoreTokens()) {
-            temp = st.nextToken();
-            if (search.equalsIgnoreCase(temp)) {
-                ++wordcount;
+            if (search.equalsIgnoreCase(st.nextToken())) {
+                wordcount++;
             }
         }
         if (wordcount == 0) {
@@ -68,6 +65,5 @@ public class Main {
             System.out.println("Word found");
             System.out.println(search + " occurs " + wordcount + " times in the file");
         }
-        return wordcount;
     }
 }
