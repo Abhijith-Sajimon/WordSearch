6ee5fa027e1596e0ce3d93a756eb7f49c5963f9a
Abhijith Sajimon <95603734+Abhijith-Sajimon@users.noreply.github.com>
2022-01-04 12:57:59 +0530

    Reduced number of methods in Database class. Declared some of the global variables locally.

--- Main.java
+++ Main.java
@@ -6,27 +6,23 @@ import java.nio.file.Paths;
 import java.sql.*;
 import java.util.StringTokenizer;
 
-import static com.company.Main.ERROR_MESSAGE;
-import static com.company.Main.statement;
-
 public class Main {
 
     static final String FILE_TYPE_TXT = ".txt";
     static final String FILE_TYPE_JSON = ".json";
     static final String REMOVE_SPECIAL_CHAR = "[^a-zA-Z0-9 -]";
     static final String SINGLE_SPACE = "";
-    static String ERROR_MESSAGE;
-    static PreparedStatement statement;
-    static String textFilePath;
-    static String searchKeyword;
-
+    static String searchResult;
 
     public static void main(String[] args) {
 
+        String textFilePath;
+        String searchKeyword;
+        String errorMessage;
         if (args.length != 2) {
-            ERROR_MESSAGE = "Provide valid filepath and search keyword";
-            System.out.println(ERROR_MESSAGE);
-            Database.wordSearchFailure(null, null, ERROR_MESSAGE);
+            errorMessage = "Provide valid filepath and search keyword";
+            System.out.println(errorMessage);
+            Database.databaseOperation(null, null, searchResult = "Failure", errorMessage, 0);
             return;
         }
         textFilePath = args[0];
@@ -34,13 +30,13 @@ public class Main {
         File file = new File(textFilePath);
         System.out.println("Processing...");
         try {
-            if (isValidFile(file)) {
+            if (isValidFile(file, textFilePath, searchKeyword)) {
                 if (file.length() != 0) {
                     wordCount(textFilePath, searchKeyword);
                 } else {
-                    ERROR_MESSAGE = "File does not contain any data";
-                    System.out.println(ERROR_MESSAGE);
-                    Database.wordSearchFailure(textFilePath, searchKeyword, ERROR_MESSAGE);
+                    errorMessage = "File does not contain any data";
+                    System.out.println(errorMessage);
+                    Database.databaseOperation(textFilePath, searchKeyword, searchResult = "Failure", errorMessage, 0);
                 }
             }
         } catch (ClassNotFoundException e) {
@@ -48,17 +44,17 @@ public class Main {
         }
     }
 
-    private static boolean isValidFile(File file) throws ClassNotFoundException {
-
+    private static boolean isValidFile(File file, String textFilePath, String searchKeyword) throws ClassNotFoundException {
+        String errorMessage;
         if (!file.exists()) {
-            ERROR_MESSAGE = "File does not exist";
-            System.out.println(ERROR_MESSAGE);
-            Database.wordSearchFailure(textFilePath, searchKeyword, ERROR_MESSAGE);
+            errorMessage = "File does not exist";
+            System.out.println(errorMessage);
+            Database.databaseOperation(textFilePath, searchKeyword, searchResult = "Failure", errorMessage, 0);
             return false;
         } else if (!(file.getName().endsWith(FILE_TYPE_TXT) || file.getName().endsWith(FILE_TYPE_JSON))) {
-            ERROR_MESSAGE = "File format not supported";
-            System.out.println(ERROR_MESSAGE);
-            Database.wordSearchFailure(textFilePath, searchKeyword, ERROR_MESSAGE);
+            errorMessage = "File format not supported";
+            System.out.println(errorMessage);
+            Database.databaseOperation(textFilePath, searchKeyword, searchResult = "Failure", errorMessage, 0);
             return false;
         }
         return true;
@@ -67,6 +63,7 @@ public class Main {
     private static void wordCount(String textFilePath, String searchKeyword) {
 
         int count = 0;
+        String errorMessage;
         String fileData;
         try {
             BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(textFilePath));
@@ -80,13 +77,13 @@ public class Main {
                 }
             }
             if (count == 0) {
-                ERROR_MESSAGE = "Word does not exist";
-                System.out.println(ERROR_MESSAGE);
-                Database.wordSearchFailure(textFilePath, searchKeyword, ERROR_MESSAGE);
+                errorMessage = "Word does not exist";
+                System.out.println(errorMessage);
+                Database.databaseOperation(textFilePath, searchKeyword, searchResult = "Failure", errorMessage, count);
             } else {
                 System.out.println("Word found");
                 System.out.println(searchKeyword + " occurs " + count + " times in the file");
-                Database.wordSearchSuccess(textFilePath, searchKeyword, count);
+                Database.databaseOperation(textFilePath, searchKeyword, searchResult = "Success", null, count);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -99,10 +96,9 @@ class Database {
     private static String CONNECTION_URL = "jdbc:mysql://localhost:3306/elixr_labs_internship";
     private static String USER = "root";
     private static String PASSWORD = "abhijith.sajimon@29";
-    private static String SEARCH_RESULT_SUCCESS = "Success";
-    private static String SEARCH_RESULT_FAILURE = "Failure";
 
-    public static void wordSearchFailure(String textFilePath, String searchKeyword, String errorMessage) {
+    public static void databaseOperation(String textFilePath, String searchKeyword, String searchResult, String errorMessage, int count) {
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -110,36 +106,16 @@ class Database {
         }
         String insertSql = "INSERT INTO audit VALUES(?,?,now(),?,?,?)";
         try {
+            PreparedStatement statement;
             Connection connect = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
             statement = connect.prepareStatement(insertSql);
             statement.setString(1, textFilePath);
             statement.setString(2, searchKeyword);
-            statement.setString(3, SEARCH_RESULT_FAILURE);
-            statement.setInt(4, 0);
-            statement.setString(5, ERROR_MESSAGE);
+            statement.setString(3, searchResult);
+            statement.setInt(4, count);
+            statement.setString(5, errorMessage);
             statement.executeUpdate();
             connect.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void wordSearchSuccess(String textFilePath, String searchKeyword, int count) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        String insertSql = "INSERT INTO audit VALUES(?,?,now(),?,?,?)";
-        try {
-            Connection connect = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
-            statement = connect.prepareStatement(insertSql);
-            statement.setString(1, textFilePath);
-            statement.setString(2, searchKeyword);
-            statement.setString(3, SEARCH_RESULT_SUCCESS);
-            statement.setInt(4, count);
-            statement.setString(5, null);
-            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
